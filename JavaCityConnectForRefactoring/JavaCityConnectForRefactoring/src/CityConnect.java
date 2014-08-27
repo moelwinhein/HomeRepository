5df51feb0265c7c43c1da92e4a8b31533ac7011a
Moe <hein.moelwin@gmail.com>
2014-08-28 01:18:28 +0800

    Make enter command message to final.

--- JavaCityConnectForRefactoring/JavaCityConnectForRefactoring/src/CityConnect.java
+++ JavaCityConnectForRefactoring/JavaCityConnectForRefactoring/src/CityConnect.java
@@ -50,6 +50,7 @@ public class CityConnect {
 	private static final String MESSAGE_INVALID_FORMAT = "invalid command format :%1$s";
 	private static final String WELCOME_MESSAGE = "Welcome to SimpleRouteStore!";
 	private static final String MESSAGE_NO_SPACE = "No more space to store locations";
+	private static final String MESSAGE_TO_ENTER_COMMAND = "Enter command:";
 
 	// These are the possible command types
 	enum COMMAND_TYPE {
@@ -100,7 +101,7 @@ public class CityConnect {
 	public static void main(String[] args) {
 		showToUser(WELCOME_MESSAGE);
 		while (true) {
-			System.out.print("Enter command:");
+			System.out.print(MESSAGE_TO_ENTER_COMMAND);
 			String userCommand = scanner.nextLine();
 			String feedback = executeCommand(userCommand);
 			showToUser(feedback);
